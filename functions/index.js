71e2f8e536874f5005a3d9127a97672233d0aab2
Oscar Andersson <oscar.andersson3@ikea.com>
2019-10-16 13:45:41 +0200

    add firebase cloud function for database connection

---
+++ functions/index.js
@@ -0,0 +1,52 @@
+const functions = require('firebase-functions');
+var admin = require("firebase-admin");
+
+//var serviceAccount = require("./env/ikea-mau-firebase-adminsdk-v92sd-d030f9b8db.json");
+
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: "https://ikea-mau.firebaseio.com"
+// });
+
+admin.initializeApp(functions.config().firebase);
+
+let db = admin.firestore();
+
+const express = require('express');
+const app = express();
+
+app.get('/getProduct/:id', async (req, res) => {
+    let catalogue = db.collection('ikea_collection').doc('catalogue');
+
+    let id = req.params.id;
+    console.log("Looking for product with id: " + id);
+    catalogue.listCollections().then(productFamilies => {
+        productFamilies.forEach(productFamily => {
+            let product = productFamily.doc(id);
+
+            product.get()
+                .then(doc => {
+                    if (!doc.exists) {
+                        console.log('No such document!');
+                    } else {
+                        console.log('Found product:', doc.data());
+                        res.json(doc.data());
+                    }
+                    return doc.data();
+                }).catch(err => {
+                    console.log('Error getting document', err);
+                    return err;
+                });
+        });
+        return productFamily;
+    }).catch(error => {
+        console.log(error);
+        return error;
+    });
+
+});
+
+app.get('/hello', (req, res) => {
+    res.sendStatus(200);
+})
+exports.api = functions.https.onRequest(app);
